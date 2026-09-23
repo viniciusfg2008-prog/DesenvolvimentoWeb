ebd0c1315433e099a300e59a369dc59480db11f3
luana <luana.a2009@aluno.ifsc.edu.br>
2026-09-23 11:03:55 -0300

    js

---
+++ js/1.js
@@ -0,0 +1,9 @@
+console.log("Olá mundão Perdido");
+//Declarar variaveis em JS. 
+i = 0; //variavel global var
+var nome="Nomelegal"; //define variavel global
+let numero; //define variavel local por bloco
+const numeroPI = 3.141516;
+
+console.log("Variavel i:" +typeof(i));
+console.log("Variavel nome:" +typeof(nome));
