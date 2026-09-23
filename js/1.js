f2318b16b3cb9e04e946a52ced70a69272165f31
COlly <Colly@gmail.com>
2026-09-23 11:02:10 -0300

    23/09

---
+++ js/1.js
@@ -0,0 +1,11 @@
+console.log("Olá Mundo");
+//Declarar variável em JS.
+i=0;  //variavel global var
+var nome = "Colly";  //define variável global
+nome = 1;
+let numero;  //define variavel local por bloco
+const numerpPI = 3.141516;
+
+
+console.log("Variável i:"+typeof(i));
+console.log("Variável i"+typeof(nome));
